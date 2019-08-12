7805ec149c4d078753d96187072c85d0e34b17a5
Al Carruth <al.carruth@gmail.com>
2019-08-12 12:11:45 -0500

    exporting simple classnames

--- index_dev.js
+++ index_dev.js
@@ -4,9 +4,9 @@ require('coffeescript/register')
 
 lib = require('./src/lib/index')
 
-exports.WS_RMI_Connection = lib.WS_RMI_Connection
-exports.WS_RMI_Object = lib.WS_RMI_Object
-exports.WS_RMI_Stub = lib.WS_RMI_Stub
+exports.Connection = lib.WS_RMI_Connection
+exports.Object = lib.WS_RMI_Object
+exports.Stub = lib.WS_RMI_Stub
 
-exports.WS_RMI_Server = lib.WS_RMI_Server
-exports.WS_RMI_Client = lib.WS_RMI_Client
+exports.Server = lib.WS_RMI_Server
+exports.Client = lib.WS_RMI_Client
