34054f8bfc9d7d18331fb386b5f8d13c9d92f032
Al Carruth <al.carruth@gmail.com>
2019-10-18 11:36:35 -0500

    fixed bug in build script

--- bin/defs.sh
+++ bin/defs.sh
@@ -4,16 +4,10 @@
 root_dir="."
 
 function clean {
-
-  pushd ${root_dir}
   rm -rf ./lib/*
   mkdir -p ./lib
-  popd
 }
 
 function build {
-
-  pushd ${root_dir}/src
-  coffee -c -o ${root_dir}/lib *.coffee
-  popd
+  coffee -c -o ./lib ./src/*.coffee
 }
