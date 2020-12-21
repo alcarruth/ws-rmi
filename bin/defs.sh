92e96f8ba8d16f137451fdc2b351d76859a2e2fd
Al Carruth <al.carruth@gmail.com>
2020-12-21 12:43:20 -0600

    add dependency 'browserify'

--- bin/defs.sh
+++ bin/defs.sh
@@ -5,17 +5,28 @@ root_dir="."
 
 function clean {
   rm -rf ./lib/ ./examples/
-  mkdir -p ./lib ./examples/stack/ipc ./examples/stack/tcp
 }
 
 function build {
+  build_lib
+  build_examples
+}
+
+function build_lib {
   echo "building ws-rmi/lib"
+  mkdir -p ./lib
   coffee -c -o ./lib ./src/lib/*.coffee > /dev/null
+  coffee -c -o . ./src/index.coffee > /dev/null
 }
 
 function build_examples {
   echo "building ws-rmi/examples"
+  mkdir -p ./examples/stack/ipc ./examples/stack/tcp
+  mkdir -p ./examples/browser/js ./examples/browser/css
   coffee -c -o ./examples/stack/ ./src/examples/stack/*.coffee
   coffee -c -o ./examples/stack/ipc/ ./src/examples/stack/ipc/*.coffee
   coffee -c -o ./examples/stack/tcp/ ./src/examples/stack/tcp/*.coffee
-}
+  cp ./src/index.html ./examples/browser
+  cp ./src/css/* ./examples/css
+  cp ./lib/ws_rmi_client.js ./lib/rmi.js ./examples/js
+  cp ./examples/stack/settings.js ./examples/js
---
+++ src/index.coffee
@@ -0,0 +1,11 @@
+# file: index.js
+# package: ws-rmi
+
+lib = require('./lib/index')
+
+exports.Connection = lib.Connection
+exports.Object = lib.Object
+exports.Stub = lib.Stub
+
+exports.Server = lib.Server
+exports.Client = lib.Client
